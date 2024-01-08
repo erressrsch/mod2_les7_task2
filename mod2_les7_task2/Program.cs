5afafa7a70e73a1f3a61c553488b747a178f1648
Szymon Chyc <erressrsch@gmail.com>
2024-01-08 23:07:04 +0100

    Add project files.

---
+++ mod2_les7_task2/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace mod2_les7_task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 15;
+            if (a % 2 == 0)
+            {
+                Console.WriteLine($"Variable {a} is even");
+            }
+            else
+            {
+                Console.WriteLine($"Variable {a} is odd");
+            }
+        }
+    }
+}
